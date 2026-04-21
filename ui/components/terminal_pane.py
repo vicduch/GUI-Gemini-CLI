e509b82f1f819dcf179ad5e2971176104e8c5b7b
vicduch <vic.duchatel@gmail.com>
2026-04-21 08:57:36 +0200

    feat: error recovery banner in terminal pane

--- ui/components/terminal_pane.py
+++ ui/components/terminal_pane.py
@@ -1,8 +1,10 @@
 import gi
 
 gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
 gi.require_version('Vte', '3.91')
-from gi.repository import Gtk, Vte
+from gi.repository import Gtk, Vte, Adw
+from core.models import ErrorContract
 
 
 class TerminalPane(Gtk.Overlay):
@@ -16,6 +18,11 @@ class TerminalPane(Gtk.Overlay):
 
         self.set_child(self.terminal)
 
+        # Bannière d'erreur (Overlay)
+        self.error_banner = Adw.Banner()
+        self.error_banner.set_valign(Gtk.Align.START)
+        self.add_overlay(self.error_banner)
+
         # Bouton Zoom Focus Mode
         self.zoom_button = Gtk.Button(icon_name="view-fullscreen-symbolic")
         self.zoom_button.set_halign(Gtk.Align.END)
@@ -24,6 +31,15 @@ class TerminalPane(Gtk.Overlay):
         self.zoom_button.set_margin_end(8)
 
         self.add_overlay(self.zoom_button)
+
+    def show_error(self, error: ErrorContract):
+        """Affiche une bannière d'erreur au-dessus du terminal."""
+        self.error_banner.set_title(f"[{error.code}] {error.message}")
+        self.error_banner.set_revealed(True)
+
+    def clear_error(self):
+        """Masque la bannière d'erreur."""
+        self.error_banner.set_revealed(False)
 
     @staticmethod
     def _create_default_terminal():
